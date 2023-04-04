780c662d280c089fa180c825f2f03dc88d802946
Samantha Genevay <samanthagenevay@gmail.com>
2023-04-04 07:59:33 -0700

    Completed Lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_list = []
+        data = json.loads(self.get_response_body())
+        for d in data:
+            data_list.append(d["name"])
+        return data_list
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+data_returned = get_requester.load_json()
+
+for data in set(data_returned):
+    print(data)
+
